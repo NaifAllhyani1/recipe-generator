c277557f46ae7b86de17005c6c8293ea4530bc0c
salman almutairi <t123y2@gmail.com>
2024-09-23 01:36:55 +0300

    - sending data to api - validate form - fixing bugs

--- frontend/src/pages/my-recipes/generate-modal.tsx
+++ frontend/src/pages/my-recipes/generate-modal.tsx
@@ -11,12 +11,16 @@ import {
 } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useUser } from "@clerk/clerk-react";
 import { useState } from "react";
+import { Recipe } from "./myRecipes";
 
 type FormData = {
   ingredients: string[] | null;
   cuisine: string | null;
   allergies: string[] | null;
+  has_all_ingredients: boolean;
+  user_id: string;
 };
 
 type ErrorData = {
@@ -25,12 +29,25 @@ type ErrorData = {
   allergies: boolean;
 };
 
-export function GenerateModal() {
+export function GenerateModal({
+  setLoadingGenerate,
+  loadingGenerate,
+  setRecipes,
+  recipes,
+}: {
+  setLoadingGenerate: (loadingGenerate: boolean) => void;
+  loadingGenerate: boolean;
+  setRecipes: (recipes: Recipe[]) => void;
+  recipes: Recipe[];
+}) {
   const [hasAllIngredients, setHasAllIngredients] = useState(false);
+  const { user } = useUser();
   const [formData, setFormData] = useState<FormData>({
     ingredients: null,
     cuisine: null,
     allergies: null,
+    has_all_ingredients: hasAllIngredients,
+    user_id: user?.id || "",
   });
 
   const [error, setError] = useState<ErrorData>({
@@ -39,47 +56,83 @@ export function GenerateModal() {
     allergies: false,
   });
 
-const validateForm = () => {
+  const validateForm = () => {
     // If user has all ingredients, validate only the cuisine
-  if (hasAllIngredients) {
-    if (!formData.cuisine || formData.cuisine.length === 0) {
-      setError((prev) => ({ ...prev, cuisine: true }));
-      return false;
+    if (hasAllIngredients) {
+      if (!formData.cuisine || formData.cuisine.length === 0) {
+        setError((prev) => ({ ...prev, cuisine: true }));
+        return false;
+      } else {
+        setError((prev) => ({ ...prev, cuisine: false }));
+      }
     } else {
-      setError((prev) => ({ ...prev, cuisine: false }));
+      // If user doesn't have all ingredients, validate only the ingredients
+      if (!formData.ingredients || formData.ingredients.length === 0) {
+        setError((prev) => ({ ...prev, ingredients: true }));
+        return false;
+      } else {
+        setError((prev) => ({ ...prev, ingredients: false }));
+      }
     }
-  } else {
-    // If user doesn't have all ingredients, validate only the ingredients
-    if (!formData.ingredients || formData.ingredients.length === 0) {
-      setError((prev) => ({ ...prev, ingredients: true }));
-      return false;
-    } else {
-      setError((prev) => ({ ...prev, ingredients: false }));
-    }
-  }
 
-  // If validation passes
-  return true;
+    // If validation passes
+    return true;
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleGenerate = async () => {
+    setLoadingGenerate(true);
 
-    const isValid = validateForm();
-    console.log(hasAllIngredients)
-    if (isValid) {
-      console.log(formData);
+    const updatedFormData = {
+      ...formData,
+      has_all_ingredients: hasAllIngredients,
+    };
 
-      console.log("Form is valid");
-    } else {
-      console.log("Form is invalid");
+    try {
+      const response = await fetch("http://127.0.0.1:8000/generate-recipe", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedFormData),
+      });
+
+      if (!response.ok) {
+        // If the response status is not OK, throw an error
+        const errorData = await response.json();
+        throw new Error(errorData.detail || "Failed to generate recipe");
+      }
+
+      const data = await response.json();
+      const newRecipe: Recipe = data.data;
+      setRecipes([newRecipe, ...recipes]);
+
+      // Handle the data (e.g., update state, display the recipe)
+    } catch (error) {
+      console.error("Error generating recipe:", error);
+      // Optionally, you can set an error state to display an error message to the user
+    } finally {
+      setLoadingGenerate(false);
     }
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const isValid = validateForm();
+
+    if (!isValid) {
+      return;
+    }
+
+    await handleGenerate();
   };
 
   return (
     <Dialog>
       <DialogTrigger asChild>
-        <Button className="bg-orange-600 hover:bg-orange-500">
+        <Button
+          className="bg-orange-600 hover:bg-orange-500"
+          disabled={loadingGenerate}
+        >
           Generate Recipe
         </Button>
       </DialogTrigger>
@@ -107,7 +160,9 @@ const validateForm = () => {
                 }
               />
               {error.ingredients && (
-                <p className="text-red-500 text-sm ">Please enter ingredients</p>
+                <p className="text-red-500 text-sm ">
+                  Please enter ingredients
+                </p>
               )}
             </div>
           )}
--- frontend/src/pages/my-recipes/myRecipes.tsx
+++ frontend/src/pages/my-recipes/myRecipes.tsx
@@ -1,9 +1,59 @@
 import { RedirectToSignIn, useAuth } from "@clerk/clerk-react";
 import { GenerateModal } from "./generate-modal";
+import { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import CardSkeleton from "./card-skeleton";
+import LoadingScreen from "@/components/loading-screen";
+
+export type CookingStep = {
+  step_number: number;
+  step_description: string;
+};
+
+export type Recipe = {
+  id: number;
+  dish_name: string;
+  cuisine: string | null;
+  dish_description: string;
+  ingredients: string[];
+  cooking_steps: CookingStep[];
+  image_url: string | null;
+  user_id: string;
+};
 
 export default function MyRecipes() {
-  // const [data, setData] = useState<any>(null);
   const { isSignedIn } = useAuth();
+  const [recipe, setRecipes] = useState<Recipe[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [loadingGenerate, setLoadingGenerate] = useState(false);
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://127.0.0.1:8000/recipes");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+        setRecipes(data.data);
+      } catch (error) {
+        console.error("Failed to fetch recipes:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchRecipes();
+  }, []);
+
   if (!isSignedIn) {
     return <RedirectToSignIn />;
   }
@@ -15,17 +65,60 @@ export default function MyRecipes() {
           <h1 className="text-3xl sm:text-4xl font-bold text-gray-800">
             My Recipes
           </h1>
-          <GenerateModal  />
+          <GenerateModal
+            setLoadingGenerate={setLoadingGenerate}
+            loadingGenerate={loadingGenerate}
+            setRecipes={setRecipes}
+            recipes={recipe}
+          />
         </div>
 
-        <div className="bg-white rounded-lg shadow-md p-8 flex flex-col items-center justify-center min-h-[300px]">
-          <h2 className="text-2xl font-semibold text-gray-600 mb-2">
-            You have no recipes yet
-          </h2>
-          <p className="text-gray-500 text-center mb-4">
-            Start by generating a new recipe or adding your own!
-          </p>
-          <GenerateModal />
+        {recipe.length === 0 && (
+          <div className="bg-white rounded-lg shadow-md p-8 flex flex-col items-center justify-center min-h-[300px]">
+            <h2 className="text-2xl font-semibold text-gray-600 mb-2">
+              You have no recipes yet
+            </h2>
+            <p className="text-gray-500 text-center mb-4">
+              Start by generating a new recipe or adding your own!
+            </p>
+            <GenerateModal
+              setLoadingGenerate={setLoadingGenerate}
+              loadingGenerate={loadingGenerate}
+              setRecipes={setRecipes}
+              recipes={recipe}
+            />
+          </div>
+        )}
+
+        {/* display recipes */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 bg-white p-4 rounded-lg">
+          {loading && <LoadingScreen />}
+          {loadingGenerate && <CardSkeleton />}
+          {recipe.map((recipe) => (
+            <Card key={recipe.id} className="flex flex-col justify-between">
+              <CardHeader>
+                <img
+                  src={recipe?.image_url!}
+                  alt={recipe?.dish_name}
+                  className="w-full h-48 object-cover rounded-lg"
+                />
+              </CardHeader>
+              <CardContent className="space-y-2 ">
+                <CardTitle className="text-lg font-semibold">
+                  {recipe.dish_name}
+                </CardTitle>
+                <p className="text-sm text-gray-500">
+                  {recipe.dish_description}
+                </p>
+              </CardContent>
+              <CardFooter className="flex justify-between">
+                <p className="text-sm text-gray-500">{recipe.cuisine}</p>
+                <Button className="bg-orange-600 hover:bg-orange-500" size="sm">
+                  View Recipe
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
         </div>
       </div>
     </main>
