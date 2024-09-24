3f2ea7fb1fb93be4f61943e641ba2a8a0a983195
salman almutairi <t123y2@gmail.com>
2024-09-24 17:31:11 +0300

    - Implemented a loader while the image is loading

--- frontend/src/components/card-item.tsx
+++ frontend/src/components/card-item.tsx
@@ -1,16 +1,26 @@
 import { Recipe } from "@/pages/my-recipes/myRecipes";
 import { Button } from "./ui/button";
-import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "./ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+import { Loader2 } from "lucide-react";
 
-export default function CardItem({recipe}: {recipe: Recipe}) {
+export default function CardItem({ recipe }: { recipe: Recipe }) {
   return (
     <Card className="flex flex-col justify-between">
       <CardHeader>
-        <img
-          src={recipe?.image_url!}
-          alt={recipe?.dish_name}
-          className="w-full h-48 object-cover rounded-lg"
-        />
+          <img
+            src={recipe?.image_url!}
+            alt={recipe?.dish_name}
+            loading="lazy"
+            className="w-full h-48 object-cover rounded-lg"
+            onLoad={() => <Loader2 className="w-10 h-10 animate-spin" />}
+          />
+        
       </CardHeader>
       <CardContent className="space-y-2 ">
         <CardTitle className="text-lg font-semibold">
